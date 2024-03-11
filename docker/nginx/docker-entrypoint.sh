1b14b78bf8fefca14e7cce441f80a3c77e8ce865
ramadan22 <harisramadan22@gmail.com>
2024-03-11 19:54:20 +0700

    feat(update): update

--- docker/nginx/docker-entrypoint.sh
+++ docker/nginx/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-set -eu
+set -e
 
-envsubst '${NODE_ENV} ${HOST} ${PORT}' < "/etc/nginx/conf.d/default.conf.template" > /etc/nginx/conf.d/default.conf
+envsubst '\$NODE_ENV \$HOST \$PORT' < "/etc/nginx/conf.d/default.conf.template" > /etc/nginx/conf.d/default.conf
 
 exec "$@"
